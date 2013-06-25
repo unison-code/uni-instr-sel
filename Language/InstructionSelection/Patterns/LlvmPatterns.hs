35d10a194b63809e00b8f5b0c9d562c0dfacb2fc
Gabriel Hjort Blindell <ghb@kth.se>
2013-06-25 15:01:37 +0200

    Added help file for exporting LLVM patterns base.

---
+++ Language/InstructionSelection/Patterns/LlvmPatterns.hs
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Language.InstructionSelection.Patterns.LlvmPatterns
+-- Copyright   :  (c) Gabriel Hjort Blindell 2013
+-- License     :  BSD-style (see the LICENSE file)
+--
+-- Maintainer  :  ghb@kth.se
+-- Stability   :  experimental
+-- Portability :  portable
+--
+--------------------------------------------------------------------------------
+
+module Language.InstructionSelection.Patterns.LlvmPatterns (
+      module Language.InstructionSelection.Patterns.LlvmPatterns.Base
+    ) where
+
+import Language.InstructionSelection.Patterns.LlvmPatterns.Base
