45efd92bdcd7d4bfa8855d5bf349cb3e1cd1c236
Gabriel Hjort Blindell <ghb@kth.se>
2013-08-12 14:04:58 +0200

    Added initial file for pattern base module.

---
+++ Language/InstructionSelection/Patterns/Base.hs
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Language.InstructionSelection.Patterns.Base
+-- Copyright   :  (c) Gabriel Hjort Blindell 2013
+-- License     :  BSD-style (see the LICENSE file)
+--
+-- Maintainer  :  ghb@kth.se
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Contains the data types and records for representing patterns. This is the
+-- format on which subsequent preparation for instruction selection will build
+-- on (i.e. other pattern forms, such as those based on LLVM, will be converted
+-- into this format).
+--
+--------------------------------------------------------------------------------
+
+{-# LANGUAGE FlexibleInstances #-}
+
+module Language.InstructionSelection.Patterns.Base where
