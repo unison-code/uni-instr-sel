92107c8a0fa17a6be03831566bfae0a651a826d0
Gabriel Hjort Blindell <ghb@kth.se>
2014-03-03 10:53:26 +0100

    Added data structures for containing the data to produce the CP model.

--- Language/InstructionSelection/CPModel/ModelToSolveData.hs
+++ Language/InstructionSelection/CPModel/ModelToSolveData.hs
@@ -14,4 +14,88 @@
 
 module Language.InstructionSelection.CPModel.ModelToSolveData where
 
--- TODO: implement
+
+
+import Language.InstructionSelection.Graphs (NodeId, Match, NodeMapping)
+
+
+
+data ModelToSolveData
+    = ModelToSolveData ProgramGraphData [PatternGraphData] MachineData
+    deriving (Show)
+
+
+
+-- | Describes the necessary program graph data.
+
+data ProgramGraphData
+    = ProgramGraphData {
+
+          -- | Number of unique nodes.
+
+          progNumUniqueNodes :: NodeId
+
+          -- | The IDs of the label nodes.
+
+        , progLabelNodes :: [NodeId]
+
+          -- | The IDs of the data nodes.
+
+        , progDataNodes :: [NodeId]
+
+          -- | The IDs of the state nodes.
+
+        , progStateNodes :: [NodeId]
+
+      }
+    deriving (Show)
+
+
+
+-- | Describes the necessary pattern graph data.
+
+data PatternGraphData
+    = PatterGraphData {
+
+          -- | Number of unique nodes.
+
+          patNumUniqueNodes :: NodeId
+
+          -- | The IDs of the label nodes.
+
+        , patLabelNodes :: [NodeId]
+
+          -- | The IDs of the use data nodes.
+
+        , patUseDataNodes :: [NodeId]
+
+          -- | The IDs of the def data nodes.
+
+        , patDefDataNodes :: [NodeId]
+
+          -- | The IDs of the use state nodes.
+
+        , patUseStateNodes :: [NodeId]
+
+          -- | The IDs of the def state nodes.
+
+        , patDefStateNodes :: [NodeId]
+
+          -- | Matches found for this pattern.
+
+        , matches :: [Match]
+
+      }
+    deriving (Show)
+
+
+
+-- | Describes the necessary target machine data.
+
+data MachineData
+    = MachineData {
+
+          -- TODO: implement
+
+      }
+    deriving (Show)
