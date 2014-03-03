8211af1b26fc2c4296f140e730d37d2ab5305465
Gabriel Hjort Blindell <ghb@kth.se>
2014-03-03 11:43:10 +0100

    Added initial structure for ParamMaker.

---
+++ Language/InstructionSelection/CPModel/ParamMaker.hs
@@ -0,0 +1,32 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Language.InstructionSelection.CPModel.ParamMaker
+-- Copyright   :  (c) Gabriel Hjort Blindell 2013
+-- License     :  BSD-style (see the LICENSE file)
+--
+-- Maintainer  :  ghb@kth.se
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- TODO
+--
+--------------------------------------------------------------------------------
+
+module Language.InstructionSelection.CPModel.ParamMaker (
+--  mkParams
+) where
+
+
+
+import Language.InstructionSelection.CPModel.Base
+import Language.InstructionSelection.Graphs.Base
+
+
+
+-------------
+-- Functions
+-------------
+
+-- | TODO: implement
+
+--mkParams :: 
