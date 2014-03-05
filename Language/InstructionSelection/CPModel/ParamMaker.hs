b6cac633d615367abbe843dc326dc97ed0958e74
Gabriel Hjort Blindell <ghb@kth.se>
2014-03-05 17:13:24 +0100

    Refactored ParamMaker module to incorporate the changes in CPModelParams. Also implemented some more stubs.

--- Language/InstructionSelection/CPModel/ParamMaker.hs
+++ Language/InstructionSelection/CPModel/ParamMaker.hs
@@ -30,32 +30,34 @@ import Language.InstructionSelection.Utils (Natural,
 -------------
 
 mkParams :: OpStructure                 -- ^ The program function.
-            -> [(OpStructure, Natural)] -- ^ The patterns.
+            -> [(OpStructure, [Match], Natural)] -- ^ The patterns.
             -> CPModelParams
 mkParams func pats =
   CPModelParams (ProgramGraphData
-                 (countNumUniqueNodeIds func)
-                 (getAllLabelNodeIds func)
+                 (mkNodePartition func)
                  (computeLabelIDomMappings func)
-                 (getAllDataNodeIds func)
-                 (getAllStateNodeIds func)
                  (osConstraints func))
                 (map mkPatternGraphData pats)
                 -- TODO: fix building of machine data
                 MachineData
 
-countNumUniqueNodeIds :: OpStructure -> Natural
-countNumUniqueNodeIds =
-  toNatural . length . removeDuplicates . allNodes . osGraph
+mkNodePartition :: OpStructure -> NodePartition
+mkNodePartition os =
+  let g = osGraph os
+  in NodePartition
+     (getUniqueNodeIdsByType g isComputationNode)
+     (getUniqueNodeIdsByType g isControlNode)
+     (getUniqueNodeIdsByType g isDataNode)
+     (getUniqueNodeIdsByType g isLabelNode)
+     (getUniqueNodeIdsByType g isPhiNode)
+     (getUniqueNodeIdsByType g isStateNode)
+     (getUniqueNodeIdsByType g isTransferNode)
 
-getAllLabelNodeIds :: OpStructure -> [NodeId]
-getAllLabelNodeIds = map (nodeId) . filter (isLabelNode) . allNodes . osGraph
+getUniqueNodeIdsByType :: Graph -> (Node -> Bool) -> [NodeId]
+getUniqueNodeIdsByType g f =
+  removeDuplicates $ map (nodeId) $ filter (f) $ allNodes g
 
-getAllDataNodeIds :: OpStructure -> [NodeId]
-getAllDataNodeIds = map (nodeId) . filter (isDataNode) . allNodes . osGraph
-
-getAllStateNodeIds :: OpStructure -> [NodeId]
-getAllStateNodeIds = map (nodeId) . filter (isStateNode) . allNodes . osGraph
+-- | TODO: write description
 
 computeLabelIDomMappings :: OpStructure -> [( NodeId -- ^ The dominator node.
                                             , NodeId -- ^ The dominated node.
@@ -64,7 +66,18 @@ computeLabelIDomMappings os =
   -- TODO: implement
   []
 
-mkPatternGraphData :: (OpStructure, Natural) -> PatternGraphData
-mkPatternGraphData (os, id) =
+mkPatternGraphData :: (OpStructure, [Match], Natural) -> PatternGraphData
+mkPatternGraphData (os, matches, id) =
+  let g = osGraph os
+  in PatternGraphData
+     (mkNodePartition os)
+     (mkUseDefs g isDataNode)
+     (mkUseDefs g isLabelNode)
+     (mkUseDefs g isStateNode)
+     (osConstraints os)
+     matches
+
+mkUseDefs :: Graph -> (Node -> Bool) -> UseDefNodes
+mkUseDefs g f =
   -- TODO: implement
-  PatternGraphData  0 [] [] [] [] [] [] [] []
+  UseDefNodes [] []
