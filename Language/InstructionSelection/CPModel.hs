5599f9fde7b9a3bc72b96181067254d247e3a0cd
Gabriel Hjort Blindell <ghb@kth.se>
2014-03-07 13:25:33 +0100

    Added CPModel.hs.

---
+++ Language/InstructionSelection/CPModel.hs
@@ -0,0 +1,17 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module      :  Language.InstructionSelection.CPModel
+-- Copyright   :  (c) Gabriel Hjort Blindell 2013
+-- License     :  BSD-style (see the LICENSE file)
+--
+-- Maintainer  :  ghb@kth.se
+-- Stability   :  experimental
+-- Portability :  portable
+--
+--------------------------------------------------------------------------------
+
+module Language.InstructionSelection.CPModel (
+  module Language.InstructionSelection.CPModel.Base
+) where
+
+import Language.InstructionSelection.CPModel.Base
