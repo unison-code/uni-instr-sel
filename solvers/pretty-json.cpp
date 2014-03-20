762a5bf817c7bdf31cf5f0cf44b36a62758df7a0
Gabriel Hjort Blindell <ghb@kth.se>
2014-03-20 11:58:31 +0100

    Added small program for pretty-printing a JSON file.

---
+++ solvers/pretty-json.cpp
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2014, Gabriel Hjort Blindell <ghb@kth.se>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "common/json/json.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace Json;
+using namespace std;
+
+int
+main (int argc, char** argv) {
+    if (argc < 2) {
+        cerr << "No JSON file." << endl;
+        return 1;
+    }
+    else if (argc > 2) {
+        cerr << "Too many arguments." << endl;
+        return 1;
+    }
+
+    ifstream file(argv[1]);
+    stringstream ss;
+    ss << file.rdbuf();
+    string data(ss.str());
+
+    Value root;
+    Reader reader;
+    if (!reader.parse(data, root)) {
+        cerr << "Failed to parse " << argv[1] << endl;
+        return 1;
+    }
+    cout << root << endl;
+    return 0;
+}
