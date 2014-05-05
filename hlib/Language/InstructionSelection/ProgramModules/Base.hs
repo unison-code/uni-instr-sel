691e49daab7954aa4251685a09f2fc711b2d1649
Gabriel Hjort Blindell <ghb@kth.se>
2014-05-05 17:48:56 +0200

    Removed the use of Module in the ProgramModules/Base, and improved the data which represent a Function.

--- hlib/Language/InstructionSelection/ProgramModules/Base.hs
+++ hlib/Language/InstructionSelection/ProgramModules/Base.hs
@@ -8,31 +8,47 @@
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- Contains the data types and records for representing program modules. This is
--- the format on which subsequent preparation for instruction selection will
--- build on (i.e. other programs forms, such as those based on LLVM, will be
--- converted into this format).
+-- Contains the data types and records for representing program modules, which
+-- basically consist of a list of functions. This is the format on which
+-- subsequent preparation for instruction selection will build on (i.e. other
+-- programs forms, such as those based on LLVM, will be converted into this
+-- format).
 --
 --------------------------------------------------------------------------------
 
 module Language.InstructionSelection.ProgramModules.Base (
-  Module (..)
-, Function (..)
+  Function (..)
 ) where
 
+import Language.InstructionSelection.Graphs
 import Language.InstructionSelection.OpStructures
+import Data.Maybe
 
 
+
+-- | The record of representing a program function.
 
 data Function
     = Function {
-          functionName :: String
-        , functionOS :: OpStructure
-      }
-    deriving (Show)
 
-data Module
-    = Module {
-          functions :: [Function]
+          -- | The function name.
+
+          functionName :: String
+
+          -- | If the function returns some data, the return type is specified
+          -- through the node in the operation structure that represents the
+          -- return data.
+
+        , functionRetType :: Maybe NodeId
+
+          -- | The nodes in the operation structure which represent the input
+          -- arguments to the function.
+
+        , functionArgs :: [NodeId]
+
+          -- | The semantics of the function.
+
+        , functionOS :: OpStructure
+
       }
     deriving (Show)
