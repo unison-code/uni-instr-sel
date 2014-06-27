9ac3d94f3c187f1eb7027d433446985f10742415
Gabriel Hjort Blindell <ghb@kth.se>
2014-06-27 13:05:45 +0200

    Added updated version of atto-lisp package in order to fix dependency problems.

---
+++ hlib/atto-lisp-0.2.1.3/dist/build/autogen/Paths_atto_lisp.hs
@@ -0,0 +1,32 @@
+module Paths_atto_lisp (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+
+version :: Version
+version = Version {versionBranch = [0,2,1,3], versionTags = []}
+bindir, libdir, datadir, libexecdir :: FilePath
+
+bindir     = "/home/ghb/.cabal/bin"
+libdir     = "/home/ghb/.cabal/lib/atto-lisp-0.2.1.3/ghc-7.4.1"
+datadir    = "/home/ghb/.cabal/share/atto-lisp-0.2.1.3"
+libexecdir = "/home/ghb/.cabal/libexec"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir :: IO FilePath
+getBinDir = catchIO (getEnv "atto_lisp_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "atto_lisp_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "atto_lisp_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "atto_lisp_libexecdir") (\_ -> return libexecdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
