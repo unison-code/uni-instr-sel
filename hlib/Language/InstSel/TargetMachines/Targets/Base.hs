2eacdcaa3f8f158e100d4877fbf4c45f59f78a20
Gabriel Hjort Blindell <ghb@kth.se>
2015-01-13 16:05:01 +0100

    Added support of copy-extending the target machines.

--- hlib/Language/InstSel/TargetMachines/Targets/Base.hs
+++ hlib/Language/InstSel/TargetMachines/Targets/Base.hs
@@ -16,7 +16,11 @@ module Language.InstSel.TargetMachines.Targets.Base
   ( getTargetMachine )
 where
 
+import Language.InstSel.Graphs.Transformations
+  ( copyExtendExcludeRootData )
 import Language.InstSel.TargetMachines.Base
+import Language.InstSel.OpStructures
+  ( OpStructure (..) )
 
 import Language.InstSel.TargetMachines.Targets.Mips32
 import Language.InstSel.TargetMachines.Targets.Test
@@ -32,6 +36,17 @@ import Language.InstSel.TargetMachines.Targets.Test
 getTargetMachine :: TargetMachineID -> Maybe TargetMachine
 getTargetMachine s =
   case (fromTargetMachineID s) of
-    "mips32" -> Just tmMips32
-    "test"   -> Just tmTest
+    "mips32"    -> Just tmMips32
+    "mips32.ce" -> Just $ copyExtendTargetMachine tmMips32
+    "test"      -> Just tmTest
     _ -> Nothing
+
+-- | Copy-extends every instruction in the given target machine.
+copyExtendTargetMachine :: TargetMachine -> TargetMachine
+copyExtendTargetMachine tm =
+  let copyExtendOS os = os { osGraph = copyExtendExcludeRootData $ osGraph os }
+      copyExtendPat p = p { patOS = copyExtendOS $ patOS p }
+      copyExtendInstr i = i { instrPatterns =
+                                map copyExtendPat (instrPatterns i)
+                            }
+  in tm { tmInstructions = map copyExtendInstr (tmInstructions tm) }
