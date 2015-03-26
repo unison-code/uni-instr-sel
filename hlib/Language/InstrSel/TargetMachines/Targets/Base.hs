28c1a530c4d3450a0c7569eee2edf8de031e3eb6
Gabriel Hjort Blindell <ghb@kth.se>
2015-03-26 17:17:59 +0100

    Copy and branch extension is now mandary part of the toolchain. Also cleaned up the Makefile a bit.

--- hlib/Language/InstrSel/TargetMachines/Targets/Base.hs
+++ hlib/Language/InstrSel/TargetMachines/Targets/Base.hs
@@ -18,8 +18,6 @@ where
 
 import Language.InstrSel.TargetMachines.Base
 import Language.InstrSel.TargetMachines.Transformations
-import Language.InstrSel.Utils
-  ( splitOn )
 
 import Language.InstrSel.TargetMachines.Targets.Mips32
 import Language.InstrSel.TargetMachines.Targets.Test
@@ -30,19 +28,12 @@ import Language.InstrSel.TargetMachines.Targets.Test
 -- Functions
 -------------
 
--- | Retrieves a target machine with a specific ID. If the name contains a dot
--- ('.'), and the suffix is "ce", then the returned target machine will have
--- been copy-extended. If no machine exists with such an identifier, 'Nothing'
--- is returned.
+-- | Retrieves a target machine with a specific ID. If no machine exists with
+-- such an identifier, 'Nothing' is returned.
 retrieveTargetMachine :: TargetMachineID -> Maybe TargetMachine
 retrieveTargetMachine tmid =
-  getTM $ splitOn "." (fromTargetMachineID tmid)
-  where getTM [m] = case m of
-                      "mips32" -> Just tmMips32
-                      "test"   -> Just tmTest
-                      _        -> Nothing
-        getTM [m, suf] = if suf == "ce"
-                         then do tm <- getTM [m]
-                                 return $ copyExtend tm
-                         else Nothing
-        getTM _ = Nothing
+  do tm <- case fromTargetMachineID tmid of
+             "mips32" -> Just tmMips32
+             "test"   -> Just tmTest
+             _        -> Nothing
+     return $ copyExtend tm
