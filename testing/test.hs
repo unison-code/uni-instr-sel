b4fea11b2ac884e616502f48e026df963bae300f
Gabriel Hjort Blindell <ghb@kth.se>
2015-03-27 13:57:51 +0100

    Removed obsolete test files.

--- testing/test.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE DeriveDataTypeable, OverloadedStrings, RecordWildCards #-}
-
-import Language.InstSel.CPModel.Json
-
-main :: IO ()
-main =
-  do json <- readFile "test.json"
-     putStrLn $ show $ fromJson json
