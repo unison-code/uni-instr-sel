31e1f644f9d5393c4f938362a35b4173f4689d77
Gabriel Hjort Blindell <ghb@kth.se>
2016-01-20 16:07:59 +0100

    Fleshed out more of the code for generating the instructions.

--- hlib/Language/InstrSel/TargetMachines/Generators/LLVM/Generator.hs
+++ hlib/Language/InstrSel/TargetMachines/Generators/LLVM/Generator.hs
@@ -15,6 +15,7 @@
 
 module Language.InstrSel.TargetMachines.Generators.LLVM.Generator where
 
+import Language.InstrSel.OpStructures.Base
 import Language.InstrSel.OpStructures.LLVM.OSMaker
 import qualified Language.InstrSel.TargetMachines.Generators.LLVM.Base as LLVM
 import qualified Language.InstrSel.TargetMachines.Base as TM
@@ -41,13 +42,51 @@ generateTargetMachine m =
 mkLocations :: LLVM.MachineDescription -> [TM.Location]
 mkLocations m =
   map processLoc $ zip ([0..] :: [Integer]) (LLVM.mdLocations m)
-  where processLoc (i, LLVM.RegLocation name value) =
-          TM.Location { TM.locID = toLocationID i
+  where processLoc (loc_id, LLVM.RegLocation name value) =
+          TM.Location { TM.locID = toLocationID loc_id
                       , TM.locName = TM.toLocationName name
                       , TM.locValue = value
                       }
 
 mkInstructions :: LLVM.MachineDescription -> [TM.Location] -> [TM.Instruction]
 mkInstructions m locs =
+  map processInstr $ zip ([0..] :: [Integer]) (LLVM.mdInstructions m)
+  where processInstr (i_id, i) =
+          TM.Instruction { TM.instrID = TM.toInstructionID i_id
+                         , TM.instrPatterns = mkInstrPatterns i
+                         , TM.instrProps = mkInstrProps i
+                         }
+
+mkInstrPatterns :: LLVM.Instruction -> [TM.InstrPattern]
+mkInstrPatterns i =
+  map processSemantics $ zip ([0..] :: [Integer]) (LLVM.instrSemantics i)
+  where processSemantics (p_num, p) =
+          let p_id = TM.toPatternID p_num
+              os = mkOpStructure p
+              tmpl = mkAsmStrTemplate os (LLVM.instrAssemblyString i)
+          in TM.InstrPattern { TM.patID = p_id
+                             , TM.patOS = os
+                             , TM.patOutputValueNodes = -- TODO: fix
+                                                        []
+                             , TM.patADDUC = True
+                             , TM.patAsmStrTemplate = tmpl
+                             }
+
+mkOpStructure :: LLVM.InstrSemantics -> OpStructure
+mkOpStructure s =
   -- TODO: implement
   undefined
+
+mkAsmStrTemplate :: OpStructure -> String -> TM.AssemblyStringTemplate
+mkAsmStrTemplate os str =
+  -- TODO: implement
+  undefined
+
+mkInstrProps :: LLVM.Instruction -> TM.InstrProperties
+mkInstrProps i =
+  TM.InstrProperties { TM.instrCodeSize = LLVM.instrSize i
+                     , TM.instrLatency = LLVM.instrLatency i
+                     , TM.instrIsNonCopy = -- TODO: fix
+                                           True
+                     , TM.instrIsNullCopy = False
+                     }
