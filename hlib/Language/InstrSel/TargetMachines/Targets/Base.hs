1a9df53fb96240835b09156f4fa4a9896c12f9b8
Gabriel Hjort Blindell <ghb@kth.se>
2016-02-08 14:33:51 +0100

    IDs are now caseinsensitive when retrieving target machines.

--- hlib/Language/InstrSel/TargetMachines/Targets/Base.hs
+++ hlib/Language/InstrSel/TargetMachines/Targets/Base.hs
@@ -18,6 +18,8 @@ where
 
 import Language.InstrSel.TargetMachines.Base
 import Language.InstrSel.TargetMachines.Transformations
+import Language.InstrSel.Utils
+  ( toLower )
 
 import qualified Language.InstrSel.TargetMachines.Targets.Mips32 as Mips32
 import qualified Language.InstrSel.TargetMachines.Targets.Test as Test
@@ -28,14 +30,15 @@ import qualified Language.InstrSel.TargetMachines.Targets.Test as Test
 -- Functions
 -------------
 
--- | Retrieves a target machine with a specific ID. If no machine exists with
--- such an identifier, 'Nothing' is returned.
+-- | Retrieves a target machine with a specific (case-insensitive) ID. If no
+-- machine exists with such an identifier, 'Nothing' is returned.
 retrieveTargetMachine :: TargetMachineID -> Maybe TargetMachine
 retrieveTargetMachine tmid =
-  do let machines = map (\t -> (tmID t, t))
+  do let machines = map (\t -> (convertID $ tmID t, t))
                         [ Mips32.theTM
-                        , Mips32.tmFancyMips32
+--                      , Mips32.tmFancyMips32
                         , Test.theTM
                         ]
-     tm <- lookup tmid machines
+     tm <- lookup (convertID tmid) machines
      return $ copyExtend tm
+  where convertID tid = toTargetMachineID $ toLower $ fromTargetMachineID tid
