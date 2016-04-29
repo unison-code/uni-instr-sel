32c97a7a709b8e117ea2ca9c597227afa226c5e4
Gabriel Hjort Blindell <ghb@kth.se>
2016-04-29 11:12:07 +0200

    Copied the functions from Graphs.Transformations into TargetMachines.Transformations.

--- hlib/instr-sel/Language/InstrSel/TargetMachines/Transformations.hs
+++ hlib/instr-sel/Language/InstrSel/TargetMachines/Transformations.hs
@@ -21,27 +21,19 @@ import Language.InstrSel.TargetMachines.Base
 import Language.InstrSel.Constraints.ConstraintBuilder
 import Language.InstrSel.DataTypes
 import Language.InstrSel.Graphs
-import Language.InstrSel.Graphs.Transformations
 import Language.InstrSel.OpStructures
   ( OpStructure (..) )
+
+import Data.Maybe
+  ( fromJust
+  , isJust
+  )
 
 
 
 -------------
 -- Functions
 -------------
-
--- | Copy-extends the given graph along every eligable data-flow edge, except
--- those edges where the value node has no definition (that is, no parents)
--- unless the value node represents a constant value.
-copyExtendGraph :: Graph -> Graph
-copyExtendGraph =
-  copyExtendWhen
-    ( \g e ->
-      let src = getSourceNode g e
-      in length (getDtFlowInEdges g src) > 0 || isValueNodeWithConstValue src
-    )
-    (\_ -> AnyType)
 
 -- | Copy-extends every instruction in the given target machine.
 copyExtend :: TargetMachine -> TargetMachine
@@ -64,3 +56,67 @@ copyExtend tm =
            else i
       new_instrs = map copyExtendInstr (tmInstructions tm)
   in tm { tmInstructions = new_instrs }
+
+-- | Inserts a copy node along every data-flow edge that involves a use of a
+-- value node except those edges where the value node has no definition (that
+-- is, no parents) unless the value node represents a constant value. This also
+-- updates the definition edges to retain the same semantics of the original
+-- graph. This means that if there is a definition edge $e$ that involves a
+-- value node used by a phi node, then upon copy extension $e$ will be moved to
+-- the new value node. Otherwise $e$ will remain on the original value
+-- node. Note that definition edges where the target is a value node are not
+-- affected.
+copyExtendGraph :: Graph -> Graph
+copyExtendGraph g =
+  let nodes = filter isValueNode (getAllNodes g)
+      edges = concatMap (getDtFlowOutEdges g) nodes
+      filtered_edges = filter ( \e ->
+                                  let src = getSourceNode g e
+                                  in length (getDtFlowInEdges g src) > 0
+                                     || isValueNodeWithConstValue src
+                              )
+                              edges
+  in foldl insertCopy g filtered_edges
+
+-- | Inserts a new copy and value node along a given data-flow edge. If the
+-- value node is used by a phi node, and there is a definition edge on that
+-- value node, then the definition edge with matching out-edge number will be
+-- moved to the new data node. Note that definition edges where the target is a
+-- value node are not affected.
+insertCopy :: Graph -> Edge -> Graph
+insertCopy g0 df_edge =
+  let old_d_node = getSourceNode g0 df_edge
+      old_d_origin = originOfValue $ getNodeType old_d_node
+      old_op_n = getTargetNode g0 df_edge
+      def_edge = if isPhiNode old_op_n
+                 then let d_node_edges = getOutEdges g0 old_d_node
+                          def_edges = filter isDefEdge d_node_edges
+                      in Just
+                         $ head
+                         $ filter (\n -> getOutEdgeNr n == getOutEdgeNr df_edge)
+                                  def_edges
+                 else Nothing
+      (g1, new_cp_node) = insertNewNodeAlongEdge CopyNode df_edge g0
+      new_dt = AnyType
+      new_origin = Just $
+                   let origins = map (fromJust . getOriginOfValueNode)
+                                     $ filter isValueNodeWithOrigin
+                                     $ getAllNodes g1
+                       prefix = if isJust old_d_origin
+                                then (fromJust old_d_origin) ++ ".copy."
+                                else "%copy."
+                   in head $ dropWhile (`elem` origins)
+                                       (map (\i -> prefix ++ show i)
+                                            ([1..] :: [Integer]))
+                                            -- Cast is needed or GHC will
+                                            -- complain...
+      (g2, new_d_node) =
+        insertNewNodeAlongEdge (ValueNode new_dt new_origin)
+                               (head $ getOutEdges g1 new_cp_node)
+                               g1
+      g3 = if isJust def_edge
+           then let e = fromJust def_edge
+                in fst $ addNewDefEdge (new_d_node, getTargetNode g2 e)
+                                       (delEdge e g2)
+           else g2
+  in g3
