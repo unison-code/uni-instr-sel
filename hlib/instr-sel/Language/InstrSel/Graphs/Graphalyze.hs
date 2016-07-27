018f62c8c70c211e86e59587484be3aba6d7efcb
Gabriel Hjort Blindell <ghb@kth.se>
2016-07-27 10:51:45 +0200

    Improved module documentation.

--- hlib/instr-sel/Language/InstrSel/Graphs/Graphalyze.hs
+++ hlib/instr-sel/Language/InstrSel/Graphs/Graphalyze.hs
@@ -2,6 +2,8 @@
 Copyright   :  Copyright (c) 2012-2016, Gabriel Hjort Blindell <ghb@kth.se>
 License     :  BSD3 (see the LICENSE file)
 Maintainer  :  ghb@kth.se
+
+Functions copied from the Graphalyze package.
 -}
 {-
 Main authors:
