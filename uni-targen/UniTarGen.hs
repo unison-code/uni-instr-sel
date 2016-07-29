c61a943f0c105426126f1951403ad3484a4aad25
Gabriel Hjort Blindell <ghb@kth.se>
2016-07-29 09:12:52 +0200

    Removed Haskell documentation headers from executable files as they break Haddock.

--- uni-targen/UniTarGen.hs
+++ uni-targen/UniTarGen.hs
@@ -1,8 +1,3 @@
-{-|
-Copyright   :  Copyright (c) 2012-2016, Gabriel Hjort Blindell <ghb@kth.se>
-License     :  BSD3 (see the LICENSE file)
-Maintainer  :  ghb@kth.se
--}
 {-
 Main authors:
   Gabriel Hjort Blindell <ghb@kth.se>
