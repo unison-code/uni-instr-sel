1ac48cbe9177541fc8198c201af2ba4672b1e31a
Gabriel Hjort Blindell <ghb@kth.se>
2016-08-08 09:48:44 +0200

    Re-generated Mips32 and Hexagon targets.

--- uni-is/UniIS/Targets/Base.hs
+++ uni-is/UniIS/Targets/Base.hs
@@ -18,6 +18,7 @@ import Language.InstrSel.Utils
   ( toLower )
 
 import qualified UniIS.Targets.Mips32 as Mips32
+import qualified UniIS.Targets.Hexagon5 as Hexagon5
 
 
 
@@ -30,7 +31,9 @@ import qualified UniIS.Targets.Mips32 as Mips32
 retrieveTargetMachine :: TargetMachineID -> Maybe TargetMachine
 retrieveTargetMachine tmid =
   do let machines = map (\t -> (convertID $ tmID t, t))
-                        [ Mips32.theTM ]
+                        [ Mips32.theTM
+                        , Hexagon5.theTM
+                        ]
      tm <- lookup (convertID tmid) machines
      return tm
   where convertID tid = toTargetMachineID $ toLower $ fromTargetMachineID tid
