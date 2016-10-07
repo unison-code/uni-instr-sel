c7afe4b82da9d71463e6b485302ee0eb6aed6dfc
Gabriel Hjort Blindell <ghb@kth.se>
2016-10-07 11:38:56 +0200

    Improved and colorized output when running checks.

--- uni-is/UniIS/Drivers/CheckIntegrity.hs
+++ uni-is/UniIS/Drivers/CheckIntegrity.hs
@@ -41,11 +41,34 @@ run
   -> IO [Output]
 
 run CheckFunctionIntegrity (Just fun) _ =
-  -- TODO: implement
-  undefined
+  do let g = osGraph $ functionOS fun
+     return $ mkOutput $
+              checkGraphInvariants g
 
 run CheckPatternIntegrity _ (Just pat) =
-  -- TODO: implement
-  undefined
+  do let g = osGraph $ patOS pat
+     return $ mkOutput $
+              checkGraphInvariants g
 
 run _ _ _ = reportErrorAndExit "CheckIntegrity: unsupported action"
+
+-- | Makes an 'Output' from a given output log. An empty log indicates no
+-- errors.
+mkOutput :: [String] -> [Output]
+mkOutput [] = [toOutput ""]
+mkOutput msgs = [toOutputWithExitCode errorExitCode (concatMap (++ "\n") msgs)]
+
+checkGraphInvariants :: Graph -> [String]
+checkGraphInvariants g =
+  checkOperationNodes g
+--  checkValueNodes g ++
+--  checkStateNodes g
+--  checkBlockNodes g ++
+--  checkControlNodes g
+
+checkOperationNodes :: Graph -> [String]
+checkOperationNodes g =
+  let ns = filter isOperationNode $
+           getAllNodes g
+      check _ msgs = msgs
+  in foldr check [] ns
