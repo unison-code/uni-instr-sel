9e02f0f363df8cbcc81b5e3a4743b6978eb86da6
Gabriel Hjort Blindell <ghb@kth.se>
2017-03-01 11:01:46 +0100

    Extended function graph checker to also check whether a datum is definable.

--- uni-is/UniIS/Drivers/CheckFunctionGraph.hs
+++ uni-is/UniIS/Drivers/CheckFunctionGraph.hs
@@ -96,8 +96,46 @@ run CheckFunctionGraphCoverage
            any (\m -> length (findPNInMatch (pmMatch m) (getNodeID n)) > 0)
                matches
          uncovered_ops = filter (not . isNodeCoverable) op_nodes
+         d_nodes = filter isDatumNode $ getAllNodes g
+         d_nodes_of_interest =
+           -- If null instructions are to be hidden, then only include data
+           -- which do not have a block node as its definer as these will never
+           -- be definable using non-null instructions
+           filter ( \n ->
+                    let es = getDtFlowInEdges g n ++ getStFlowInEdges g n
+                    in not hide_null_instrs &&
+                       not ( length es == 1 &&
+                             isBlockNode (getSourceNode g $ head es)
+                           )
+                  ) $
+           d_nodes
+         isDatumDefinable n =
+           any ( \m ->
+                 let p = do let os = getPatternGraph tm m
+                                pg = osGraph os
+                            pn_id <- let ns = findPNInMatch (pmMatch m)
+                                                            (getNodeID n)
+                                     in if length ns > 0
+                                        then Just $ head ns
+                                        else Nothing
+                            pn <- let ns = findNodesWithNodeID pg pn_id
+                                  in if length ns > 0
+                                     then Just $ head ns
+                                     else error $ "run: found no pattern " ++
+                                                  "node in match " ++
+                                                  pShow (pmMatchID m) ++
+                                                  " with node ID " ++
+                                                  pShow pn_id
+                            return $ if isValueNode pn
+                                     then length (getDtFlowInEdges pg pn) > 0
+                                     else length (getStFlowInEdges pg pn) > 0
+                 in if isJust p then fromJust p else False
+               )
+               matches
+         undefined_ds = filter (not . isDatumDefinable) d_nodes_of_interest
          l = concatLogs $
-             map (reportUncoveredOp g) uncovered_ops
+             map (reportUncoveredOp g) uncovered_ops ++
+             map reportUndefinableDatum undefined_ds
      return $ mkOutputFromLog l
 
 run CheckFunctionGraphLocationOverlap _ _ function matchset (Just tm) =
@@ -109,12 +147,6 @@ run CheckFunctionGraphLocationOverlap _ _ function matchset (Just tm) =
       g = osGraph $ functionOS function
       data_nodes = filter isDatumNode $ getAllNodes g
       matches = pmMatches matchset
-      getPatternGraph pm =
-        let i = findInstruction tm (pmInstrID pm)
-            ip = findInstrPattern (instrPatterns $ fromJust i) (pmPatternID pm)
-        in if isJust i && isJust ip
-           then patOS $ fromJust $ ip
-           else error "run: no instruction or pattern with matching ID"
       getDefLocationsForNode os nid =
         let ssa_g = extractSSA $ osGraph os
             n = head $ findNodesWithNodeID ssa_g nid
@@ -133,7 +165,7 @@ run CheckFunctionGraphLocationOverlap _ _ function matchset (Just tm) =
               S.unions $
               map ( \pm -> let m = pmMatch pm
                                pn = findPNInMatch m nid
-                               os = getPatternGraph pm
+                               os = getPatternGraph tm pm
                                locs = map ( S.fromList .
                                             getDefLocationsForNode os
                                           )
@@ -145,7 +177,7 @@ run CheckFunctionGraphLocationOverlap _ _ function matchset (Just tm) =
               S.unions $
               map ( \pm -> let m = pmMatch pm
                                pn = findPNInMatch m nid
-                               os = getPatternGraph pm
+                               os = getPatternGraph tm pm
                                locs = map ( S.fromList .
                                             getUseLocationsForNode os
                                           )
@@ -180,11 +212,25 @@ reportUncoveredOp g o =
      "Uncovered operation: " ++ pShow o ++ " with in-data " ++ pShow in_ds ++
      " and out-data " ++ pShow out_ds
 
+reportUndefinableDatum :: Node -> Log
+reportUndefinableDatum d =
+  toLog $
+  ErrorMessage $
+  "Undefinable datum: " ++ pShow d
+
 reportNonOverlappingLocsForDatum :: Node -> Log
 reportNonOverlappingLocsForDatum n =
   toLog $
   ErrorMessage $
   "Datum node with non-overlapping location requirements: " ++ pShow n
+
+getPatternGraph :: TargetMachine -> PatternMatch -> OpStructure
+getPatternGraph tm pm =
+  let i = findInstruction tm (pmInstrID pm)
+      ip = findInstrPattern (instrPatterns $ fromJust i) (pmPatternID pm)
+  in if isJust i && isJust ip
+     then patOS $ fromJust $ ip
+     else error "run: no instruction or pattern with matching ID"
 
 filterMatches
   :: TargetMachine
